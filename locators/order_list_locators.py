52d7a92514e490f7cc235e7c3a5511b96962655f
Forcecom92 <4621389@gmail.com>
2025-09-15 13:13:57 +0300

    Addings locators pages

---
+++ locators/order_list_locators.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+BUTTON_ORDER_LIST = (By.XPATH, "//p[text()='Лента Заказов']")
+BUTTON_ACCOUNT = (By.XPATH, "//*[contains(text(), 'Личный Кабинет')]")
+BUTTON_ENTER = (By.XPATH, "//button[text()='Войти']")
+BUTTON_MAKE_ORDER = (By.XPATH, "//button[text()='Оформить заказ']")
+BUTTON_HISTORY_PROFILE = (By.XPATH, "//a[text()='История заказов']")
+INGREDIENT = (By.XPATH, "//a[@href='/ingredient/61c0c5a71d1f82001bdaaa6c']/div/p[@class='counter_counter__num__3nue1']")
+ORDER_NUMBER_CARD = (By.XPATH, "(//p[@class='text text_type_digits-default'])[1]")
+ORDER_COUNT_TOTAL = (By.XPATH, "//div[@class='undefined mb-15']/p[@class='OrderFeed_number__2MbrQ text text_type_digits-large']")
+ORDER_COUNT_DAY = (By.XPATH, "(//div/p[@class='OrderFeed_number__2MbrQ text text_type_digits-large'])[2]")
+ORDER_BASKET = (By.XPATH, "//ul[@class='BurgerConstructor_basket__list__l9dp_']")
+ORDER_CARD = (By.XPATH, "(//a[@class='OrderHistory_link__1iNby'])[1]")
+ORDER_CARD_MODAL_WINDOW_CROSS = (By.XPATH, "//button[@class='Modal_modal__close_modified__3V5XS Modal_modal__close__TnseK']")
+ORDER_CARD_MODAL_WINDOW = (By.XPATH, "//div[@class='Modal_orderBox__1xWdi Modal_modal__contentBox__sCy8X p-10']")
+LEFT_BLOCK = (By.XPATH, "//ul[@class='OrderFeed_list__OLh59']")
+NUMBER_IN_WORK = (By.XPATH, "(//li[contains(@class, 'text text_type_digits-default mb-2')])[6][1]")
---
+++ locators/personal_account_locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+BUTTON_ACCOUNT = (By.XPATH, "//*[contains(text(), 'Личный Кабинет')]")
+BUTTON_HISTORY_PROFILE = (By.XPATH, "//a[text()='История заказов']")
+BUTTON_SAVE = (By.XPATH, "//button[@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa']")
+INPUT_EMAIL = (By.XPATH, "//label[text() = 'Email']/../input")
+INPUT_PASSWORD = (By.XPATH, "//label[text() = 'Пароль']/../input")
+EXIT_BUTTON = (By.XPATH, "//button[text()='Выход']")
+BUTTON_ENTER = (By.XPATH, "//button[text()='Войти']")
